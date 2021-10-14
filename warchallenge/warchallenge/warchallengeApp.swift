f2652e92dfd3efc96254e463afba322c83b65888
Sarowar-Alam-Saidi <sarowaralam47@gmail.com>
2021-10-14 22:32:59 +0600

    Add War Challange app with SwiftUI

---
+++ warchallenge/warchallenge/warchallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  warchallengeApp.swift
+//  warchallenge
+//
+//  Created by Md Shah Alam on 10/14/21.
+//
+
+import SwiftUI
+
+@main
+struct warchallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
